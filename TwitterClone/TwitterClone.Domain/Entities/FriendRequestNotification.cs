0bad711b4e69918698352d8d2f3f731d88030b45
Sanzida <tabassumsts556@gmail.com>
2026-08-27 02:07:37 +0600

    Class 9 Apply abstraction on notification classes

---
+++ TwitterClone/TwitterClone.Domain/Entities/FriendRequestNotification.cs
@@ -0,0 +1,19 @@
+﻿namespace TwitterClone.Domain.Entities
+{
+    public class FriendRequestNotification : Notification
+    {
+        public FriendRequestNotification() : base()
+        {
+        }
+
+        public override string GetNotificationMessage()
+        {
+            return "You received a new friend request.";
+        }
+
+        public override string DescribeRecord()
+        {
+            return base.DescribeRecord();
+        }
+    }
+}
